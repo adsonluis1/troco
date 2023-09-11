e273d02ea4cd1c1cf8122217a6fbd2ad3e46b4a3
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-09-11 19:08:35 -0300

    a

--- index.ts
+++ index.ts
@@ -2,13 +2,13 @@ let entrada = 0
 
 function trocado(preço:number , pagamento:number){
     // moedas aceitas = 0.05 , 0.10 , 0.25 , 0.50 , 1.0
-    const troco:number = preço - pagamento
+    let troco:number = preço - pagamento
     let cinquenta:number = 0
     if(troco === 0){
         console.log('certinho')
     }else{
        while(troco - 1 > 1){
-            troco == troco - 1
+            troco = troco - 1
             console.log(troco) 
        }
     }
