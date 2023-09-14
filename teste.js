6525c1255f2af8721f030879873ba6ccb6130d37
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-09-14 19:31:53 -0300

    teste

---
+++ teste.js
@@ -0,0 +1,18 @@
+"use strict";
+const pessoa = {
+    nome: 'adson',
+    sobrenome: 'luis',
+    idade: 18,
+    hobby: 'programar'
+};
+const { nome, sobrenome, idade, hobby } = pessoa;
+const fullname = `${nome} ${sobrenome}`;
+console.log(nome);
+console.log(sobrenome);
+console.log(idade);
+console.log(hobby);
+console.log(fullname);
+const arr = ['adson', 'anne', 'rafaelle'];
+const [a, , b] = arr;
+console.log(a);
+console.log(b);
